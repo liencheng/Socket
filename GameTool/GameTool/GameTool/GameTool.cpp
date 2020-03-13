c3bd35f9f4a8e0c3da3c508feb2251c8ac5624fe
liencheng <281225852@qq.com>
2020-03-13 20:17:15 +0800

    增加机器人TIck收发socket逻辑

--- GameTool/GameTool/GameTool/GameTool.cpp
+++ GameTool/GameTool/GameTool/GameTool.cpp
@@ -6,29 +6,21 @@
 
 using namespace std;
 
-#define THREAD_POOL_COUNT (10)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	MyThreadPool threadPool(THREAD_POOL_COUNT);
-
-	MyThread my;
-	my.StartThread();
-
+	cout << "main thread pid:" << GetCurrentThreadId() << endl;
 	int nTickCt = 0;
 	ToolManager toolMgr;
 	toolMgr.InitNetWork();
 	toolMgr.InitAddr();
 	toolMgr.InitListenSocket();
-
-
-
 	cout << "main tread start" << endl;
-
 	while (true)
 	{
 		toolMgr.Tick();
 	};
+
 	return 0;
 }
 
--- GameTool/GameTool/GameTool/ThreadPool.cpp
+++ GameTool/GameTool/GameTool/ThreadPool.cpp
@@ -1,7 +1,9 @@
 #include "stdafx.h"
 #include "ThreadPool.h"
 #include <iostream>
+#include "winsock2.h"
 #include "windows.h"
+
 
 using namespace std;
 
@@ -16,18 +18,105 @@ void MyThread::Tick()
 	int idx = 0;
 	while (true)
 	{
-		Sleep(1000);
 		ReqUser();
+		Tick_ProcessSocket();
+		Tick_ProcessInput();
+		Tick_ProcessOutput();
 	};
+
 }
 
 void MyThread::ReqUser()
 {
 	ClientUser rUser(-1, MySocket());
-	//if (m_ThreadPool->PopUser(&rUser))
+	if (m_ThreadPool->PopUser(&rUser))
 	{
 		m_UserVec.push_back(rUser);
+		cout << "pid:" << GetCurrentThreadId() << endl;
+	}
+}
 
-		cout << "add user to thread succ. count:" << m_UserVec.size() << endl;
+void MyThread::Tick_ProcessSocket()
+{
+	if (m_UserVec.size() <= 0)
+	{
+		return;
+	}
+	FD_ZERO(&m_fs_read);
+	FD_ZERO(&m_fs_write);
+	FD_ZERO(&m_fs_exception);
+
+	for (int idx = 0; idx < m_UserVec.size(); ++idx)
+	{
+		FD_SET(m_UserVec[idx].m_socket.m_socket, &m_fs_read);
+		FD_SET(m_UserVec[idx].m_socket.m_socket, &m_fs_write);
+		FD_SET(m_UserVec[idx].m_socket.m_socket, &m_fs_exception);
+	}
+	timeval tv;
+	tv.tv_usec = 0;
+	tv.tv_sec = 0;
+	int nRet = select(-1, &m_fs_read, &m_fs_write, &m_fs_exception, &tv);
+
+	if (nRet == SOCKET_ERROR)
+	{
+		cout << "query sockets error" << endl;
+		return;
+	}
+}
+
+void MyThread::Tick_ProcessInput()
+{
+	int nValidSock = 0;
+	for (int idx = 0; idx < m_UserVec.size(); ++idx)
+	{
+		SOCKET sock = m_UserVec[idx].m_socket.m_socket;
+		if (FD_ISSET(sock, &m_fs_read))
+		{
+			char buf[2048];
+			memset(buf, 0, sizeof(buf));
+			int recvlen = recv(sock, buf, sizeof(buf), 0);
+
+			if (recvlen == 0)
+			{
+				cout << "socket has been closed. sock:" << sock << endl;
+			}
+			else if (recvlen == SOCKET_ERROR)
+			{
+				cout << "socket error. sock:" << sock << endl;
+			}
+			else
+			{
+				nValidSock++;
+				char* outbuf = new char[recvlen + 1];
+				memcpy(outbuf, buf, recvlen);
+				outbuf[recvlen] = 0;
+				cout << "recv data," << outbuf << " ,pid:"<<GetCurrentThreadId()<<endl;
+				delete outbuf;
+			}
+		}
+	}
+}
+
+void MyThread::Tick_ProcessOutput()
+{
+	for (int idx = 0; idx < m_UserVec.size(); ++idx)
+	{
+		SOCKET sock = m_UserVec[idx].m_socket.m_socket;
+		if (FD_ISSET(sock, &m_fs_write))
+		{
+			char buf[1024] = "ku wa yi madai";
+			int nRet = send(sock, buf, sizeof(buf), 0);
+			if (nRet == 0)
+			{
+				cout << "socket closed, sock:" << sock << endl;
+			}
+			else if (nRet == SOCKET_ERROR)
+			{
+				cout << "send error, sock:" << sock << endl;
+			}
+			else{
+				cout << "send, sock:" << sock << endl;
+			}
+		}
 	}
 }
